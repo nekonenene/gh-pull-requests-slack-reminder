6c337c1c5b9de8e89f83c54d54f7e2ae2eda01b6
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-11-28 23:38:00 +0900

    Check if today is weekend

--- cmd/executor.go
+++ cmd/executor.go
@@ -1,5 +1,30 @@
 package cmd
 
+import (
+	"fmt"
+	"time"
+)
+
 func Exec() {
 	ParseParameters()
+
+	if params.AvoidWeekend && isWeekend() {
+		fmt.Printf("\033[33mSkipped because today is a weekend.\033[0m\n")
+		return
+	}
+
+	if params.AvoidJapaneseHolidays && isJapaneseHoliday() {
+		fmt.Printf("\033[33mSkipped because today is a Japanese holiday.\033[0m\n")
+		return
+	}
+}
+
+func isWeekend() bool {
+	currentTime := time.Now()
+
+	return currentTime.Weekday() == time.Saturday || currentTime.Weekday() == time.Sunday
+}
+
+func isJapaneseHoliday() bool {
+	return true // TODO
 }
